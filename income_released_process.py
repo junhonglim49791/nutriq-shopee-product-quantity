7246d9290ce551ea1f082c01946e7a25b4dbba28
junhonglim49791 <limjunhong4979@gmail.com>
2025-07-26 20:47:06 +0800

    Commented income_released_process.py

--- income_released_process.py
+++ income_released_process.py
@@ -16,6 +16,7 @@ from datetime import datetime, timedelta
 #            )
 
 
+# Get Order Info part for stock count, others are unnecessary
 def get_processed_income_released_df(income_released_file_path):
     income_released_df = pd.read_excel(
         income_released_file_path,
@@ -44,7 +45,7 @@ def get_processed_income_released_df(income_released_file_path):
         ],
     ]
 
-    # Shopee sorted by default?
+    # Shopee sorted by default? Yes, but just in case
     sorted_final_income_df = final_income_df.sort_values(
         by=("Order Info", "Unnamed: 6_level_1", "Order Creation Date")
     )
@@ -56,24 +57,24 @@ def get_income_released_order_ids(processed_income_released_df):
     return processed_income_released_df[order_ids_header]
 
 
-"""Get list of tuples. where tuple('MM', [list of unique dates])"""
-
-
 # "Order Creation Date" is str type
 # print(
 #     final_income_df["Order Info", "Unnamed: 6_level_1", "Order Creation Date"]
 #     .map(type)
 #     .unique()
 # )
+"""Get list of tuples. where tuple('YYYY-MM', [list of unique dates]), to determine the filenames of Order.Completed"""
+
+
 def get_unique_year_month_list(final_income_df):
     order_creation_date_header = (
         "Order Info",
         "Unnamed: 6_level_1",
         "Order Creation Date",
     )
-    # The values for 'Order Creation Date' colum is in %Y-%m-%d. Getting YYYY-MM as key helps with sorting the dates in desc order.
+    # The values for 'Order Creation Date' colum is in %Y-%m-%d. Getting YYYY-MM using str.slice(0,7) as key helps with sorting the dates in desc order.
     # Using month only for the key will not sort properly when there is a case of '12' (Dec) in 2024, and the next month is '01' Jan in 2025.
-    # Sort in desc because I found that the latest month will always have more orders, trying to make use it as a base datafram to be expanded with earlier months.
+    # Sort in desc because I found that the latest month will always have more orders, trying to make use it as a base dataframe to be expanded with earlier months.
     unique_year_month_list = (
         final_income_df[order_creation_date_header].str.slice(0, 7).unique()
     )
@@ -125,11 +126,6 @@ def get_required_order_completed_filename(unique_year_month_list):
             date_before30days_from_current_year_month_end_date.strftime("%Y-%m")
         )
 
-        # print(
-        #     "Date before 30 days: ",
-        #     date_before30days_from_current_year_month_end_date_str,
-        # )
-
         # if is within the current month, no need to find new start date from previous month because there is already 30 days orders data in this month
         if date_before30days_from_current_year_month == current_year_month_str:
             continue
@@ -149,7 +145,6 @@ def get_required_order_completed_filename(unique_year_month_list):
             new_index_current_month_start_date = prev_month_dates_list.index(
                 date_before30days_from_current_year_month_end_date_str
             )
-            # print("in list")
         else:
             # Check whether there is a date from previous month which is later than the date_before30days_from_current_year_month_end_date_str.
             # This means that I can take that date as a new start date for current month
@@ -159,19 +154,16 @@ def get_required_order_completed_filename(unique_year_month_list):
             and in '2025-04', ['2025-04-25', '2025-04-26', '2025-04-27'(imagine is here for explanation),'2025-04-28'],
             Since '2025-04-28' is in the 30 days range, '2025-04-26' is out of 30 days range. So user should download from '2025-04-28, instead of '2025-05-01'
             """
-            # print("NOT in list")
             for i, date in enumerate(prev_month_dates_list):
                 if date > date_before30days_from_current_year_month_end_date_str:
                     new_index_current_month_start_date = i
-                    # Stop the lost, only the next date is needed, if not index every date that is later than date_before30days will be set to new_index
+                    # Stop the loop, only the next date is needed, if not index every date that is later than date_before30days will be set to new_index
                     break
 
         # Just skip if there is no date within the range of date_before30days_from_current_year_month_end_date_str to current month's end date,
         if new_index_current_month_start_date is None:
             continue
-        # print("Before slice (current_month): ", current_year_month_dates_list)
-        # print("Before slice (previous_month): ", prev_month_dates_list)
-        # print("New index: ", new_index_current_month_start_date)
+
         """Example
         if in '2025-05' , ['2025-05-01', ... '2025-05-27'] <--- last date in May,
         '2025-04-27' is date_before30days_from_current_year_month_end_date_str,
@@ -192,8 +184,6 @@ def get_required_order_completed_filename(unique_year_month_list):
             :new_index_current_month_start_date
         ]
 
-        # print("After slice: ", current_year_month_dates_list)
-        # print("After slice: ", prev_month_dates_list)
     completed_order_filenames = []
     for year_month, datelist in unique_year_month_list:
         if len(datelist) != 0:
